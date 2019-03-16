0029bb1b1b82b3ef02eb66b65d9ae1118868d501
nanomishka <mihail777str@yandex.ru>
2019-03-17 02:21:50 +0300

    submission.py

--- jupyter_english/assignments_spring2019/kaggle_alice_mikhail_yuldashev_solution.py
+++ jupyter_english/assignments_spring2019/kaggle_alice_mikhail_yuldashev_solution.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 PATH_TO_DATA = '../../data/'
-AUTHOR = 'Yury_Kashnitskiy'
+AUTHOR = 'mikhail_yuldashev'
 SEED = 17
 N_JOBS = 4
 NUM_TIME_SPLITS = 10    # for time-based cross-validation
@@ -23,9 +23,9 @@ SITE_NGRAMS = (1, 2)    # site ngrams for "bag of sites"
 DF_MAX = 0.8
 DF_MIN = 2
 MAX_FEATURES = 50000    # max features for "bag of sites"
-BEST_LOGIT_C = 5.45559  # precomputed tuned C for logistic regression
+BEST_LOGIT_C = 12.915496650148826  # precomputed tuned C for logistic regression
 
- 
+
 
 @contextmanager
 def timer(name):
@@ -66,17 +66,23 @@ def add_features(times, X_sparse):
     day = ((hour >= 12) & (hour <= 18)).astype('int')
     evening = ((hour >= 19) & (hour <= 23)).astype('int')
     
-    # sess_duration = (times.max(axis=1) - times.min(axis=1)).astype('timedelta64[s]')\
-		  #  .astype('int').values.reshape(-1, 1)
-    # day_of_week = times['time1'].apply(lambda t: t.weekday()).values.reshape(-1, 1)
-    # month = times['time1'].apply(lambda t: t.month).values.reshape(-1, 1) 
-    # year_month = times['time1'].apply(lambda t: 100 * t.year + t.month).values.reshape(-1, 1) / 1e5
+    duration = (times.max(axis=1) - times.min(axis=1)).astype('timedelta64[s]') / 1800
+    day_of_week = times['time1'].apply(lambda t: t.weekday())
+    back = times.count(axis=1) - times.nunique(axis=1)
+    avg_time = times.apply(
+        lambda x: x.apply(lambda v: int(v.timestamp()) if not pd.isnull(v) else v)
+        ).diff(axis=1).median(axis=1, skipna=True).fillna(0)
+    y2014 = (times['time1'].apply(lambda ts: ts.year) == 2014).astype('int')
+
+    hours_start = pd.get_dummies(hour)
+    end_hour = times.max(axis=1).apply(lambda ts: ts.hour)
+    hours_end = pd.get_dummies(end_hour)
 
     X = hstack([
         X_sparse,
         *[f.values.reshape(-1, 1) for f in
-            (hour, morning, day, evening)
-        ]])
+            (hour, morning, day, evening, duration, day_of_week, back, avg_time, y2014)
+        ], hours_start, hours_end])
     
     return X
 
@@ -97,23 +103,16 @@ with timer('Building additional features'):
     X_test_final = add_features(test_times, X_test_sites)
 
 
-# with timer('Cross-validation'):
-#     time_split = TimeSeriesSplit(n_splits=NUM_TIME_SPLITS)
-#     logit = LogisticRegression(random_state=SEED, solver='liblinear')
-
-#     # I've done cross-validation locally, and do not reproduce these heavy computations here,
-#     # but this is the vest C that I've found
-#     c_values = [BEST_LOGIT_C]
-
-#     logit_grid_searcher = GridSearchCV(estimator=logit, param_grid={'C': c_values},
-#                                   scoring='roc_auc', n_jobs=N_JOBS, cv=time_split, verbose=1)
-#     logit_grid_searcher.fit(X_train_final, y_train)
-#     print('CV score', logit_grid_searcher.best_score_)
+with timer('Cross-validation'):
+    time_split = TimeSeriesSplit(n_splits=NUM_TIME_SPLITS)
+    logit = LogisticRegression(random_state=SEED, solver='liblinear', C=BEST_LOGIT_C)
+    logit.fit(X_train_final, y_train)
 
 
-# with timer('Test prediction and submission'):
-#     test_pred = logit_grid_searcher.predict_proba(X_test_final)[:, 1]
-#     pred_df = pd.DataFrame(test_pred, index=np.arange(1, test_pred.shape[0] + 1),
-#                        columns=['target'])
-#     pred_df.to_csv(f'submission_alice_{AUTHOR}.csv', index_label='session_id')
+with timer('Test prediction and submission'):
+    test_pred = logit.predict_proba(X_test_final)[:, 1]
+    pred_df = pd.DataFrame(test_pred, index=np.arange(1, test_pred.shape[0] + 1),
+                       columns=['target'])
+    pred_df.to_csv(f'submission_alice_{AUTHOR}.csv', index_label='session_id')
+
 
